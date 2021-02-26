6d8f67c9bde1eb235a8567e4d7e2975dff12fc8e
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-26 12:58:49 +0100

    db connection test

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const app = express();
-
 const dotenv = require("dotenv");
 dotenv.config();
 
+const express = require("express");
+const pool = require("./dbconfig");
+const app = express();
+
+
 const messagesRoutes = require("./Routes/messages");
+
 app.use("/messages", messagesRoutes);
+app.get("/usertest", async (req, res) => {
+  const result = await pool.query("SELECT * FROM users");
+  res.send(result);
+});
 app.get("/", (_, res) => {
   res.send("hello there");
 });
