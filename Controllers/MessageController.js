83f5f096293ae71e35bf25ada47f208e1562162a
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-02-26 14:51:54 +0100

    add database connection for messages

--- Controllers/MessageController.js
+++ Controllers/MessageController.js
@@ -1,100 +1,89 @@
-//const pool = require("../dbconfig");
+const pool = require("../dbconfig");
 
-placeholderMessages = [
-  {
-    id: 1,
-    text: "some sample text",
-    date: new Date().getUTCDate,
-    user: {
-      id: 1,
-      name: "user1",
-      email: "someEmail",
-    },
-  },
-  {
-    id: 2,
-    text: "some sample text2",
-    date: new Date().getUTCDate,
-    user: {
-      id: 1,
-      name: "user1",
-      email: "someEmail",
-    },
-  },
-  {
-    id: 1,
-    text: "some sample text",
-    date: new Date().getUTCDate,
-    user: {
-      id: 1,
-      name: "user1",
-      email: "someEmail",
-    },
-  },
-  {
-    id: 2,
-    text: "some sample text2",
-    date: new Date().getUTCDate,
-    user: {
-      id: 1,
-      name: "user1",
-      email: "someEmail",
-    },
-  },
-  {
-    id: 1,
-    text: "some sample text",
-    date: new Date().getUTCDate,
-    user: {
-      id: 2,
-      name: "user2",
-      email: "someEmail2",
-    },
-  },
-  {
-    id: 2,
-    text: "some sample text2",
-    date: new Date().getUTCDate,
-    user: {
-      id: 2,
-      name: "user2",
-      email: "someEmail2",
-    },
-  },
-];
+const messageQuery =
+  "SELECT messages.id, messages.text, messages.date, row_to_json(users.*) AS user " +
+  "FROM messages JOIN users ON (id_user=users.id)";
 
 module.exports = {
   getMessages: async (_, res) => {
-    /*try {
-        const answerDB = await pool.query('SELECT * FROM messages, user WHERE user ,
-        [first_name, last_name, age, id]);
-
-    } catch(e) {
-        console.error(e);
-        res.json({
-            message: "unexpected error",
-            code: 500,
-        })
-    }*/
-    res.json({
-      message: "get messages",
-      code: 200,
-      data: placeholderMessages,
-    });
+    try {
+      const answerDB = await pool.query(messageQuery);
+      res.json({
+        message: "get messages",
+        code: 200,
+        data: answerDB.rows,
+      });
+    } catch (e) {
+      console.error(e);
+      res.json({
+        message: "unexpected error",
+        code: 500,
+      });
+    }
   },
   getMessagebyId: async (req, res) => {
     const { id } = req.params;
-    res.json({
-      message: "get message with id:" + id,
-      code: 200,
-      data: placeholderMessages[id + 1],
-    });
+
+    if (!/^\d+$/.test(id)) {
+      res.status(400).json({
+        code: 400,
+        message: id + " is not a valid ID",
+      });
+      return;
+    }
+
+    try {
+      const answerDB = await pool.query(
+        `${messageQuery} WHERE messages.id = $1`,
+        [id]
+      );
+
+      if (!answerDB.rows.length) {
+        res.json({
+          message: "message not found",
+          code: 404,
+          data: answerDB.rows,
+        });
+        return;
+      }
+      res.json({
+        message: "get messages",
+        code: 200,
+        data: answerDB.rows[0],
+      });
+    } catch (e) {
+      console.error(e);
+      res.json({
+        message: "unexpected error",
+        code: 500,
+      });
+    }
   },
   createMessage: async (req, res) => {
-    const { text, date, user_id } = req.body;
-    res.json({
-      message: `message inserted: text: ${text}, date: ${date}, user_id: ${user_id}  (stil to be done`,
-      code: 200,
-    });
+    const { text, user_id } = req.body;
+
+    if (!text || !user_id) {
+      res.status(400).json({
+        code: 400,
+        message: "message not valid",
+      });
+      return;
+    }
+    try {
+      const answerDB = await pool.query(
+        "INSERT INTO messages (text, date, id_user) VALUES ( $1, NOW(), $2)",
+        [text, user_id]
+      );
+      res.json({
+        message: `message inserted: text: ${text}, user_id: ${user_id}`,
+        code: 200,
+      });
+    } catch (e) {
+      console.error(e);
+      res.json({
+        message: "unexpected error",
+        code: 500,
+      });
+    }
   },
 };
